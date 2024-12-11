576e12b512c44ebb5e43f2b68e1f76fd79305c49
SamanMau <143556055+SamanMau@users.noreply.github.com>
2024-12-11 12:29:57 +0100

    Lägger in filer

---
+++ Twitetr/src/main/java/com/example/Twitetr/TwitetrApplication.java
@@ -0,0 +1,13 @@
+package com.example.Twitetr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitetrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitetrApplication.class, args);
+	}
+
+}
