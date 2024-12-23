eb4bc33132c77954d49c8f6ace6f9f00995acd46
SamanMau <saman_jejo@hotmail.com>
2024-12-23 17:06:05 +0100

    Ändrade om alla ord från "tweet" till "thread"

--- Twitetr/src/main/java/com/example/Twitetr/Controller/Controller_Thread.java
+++ Twitetr/src/main/java/com/example/Twitetr/Controller/Controller_Thread.java
@@ -1,9 +1,6 @@
 package com.example.Twitetr.Controller;
 
-//URL FÖR ATT TESTA BACKEND: http://localhost:8080/api/tweets
-//http://localhost:8080/api/tweets
-
-import com.example.Twitetr.Entity.Tweet;
+import com.example.Twitetr.Entity.Thread;
 import com.example.Twitetr.Service.LibrisManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,64 +16,53 @@ import java.util.regex.Pattern;
 
 @RestController
 @CrossOrigin
-@RequestMapping("/api/tweets")
-public class Controller_Twitter {
-    private ArrayList<Tweet> tweetList = new ArrayList<>();
+@RequestMapping("/api/threads")
+public class Controller_Thread {
+    private ArrayList<Thread> threadList = new ArrayList<>();
     @Autowired
     private LibrisManager libris;
 
-    @GetMapping //Hämtar alla Tweets
-    public String getAllTweets() { //testmetod.
-        return "Hej hej!";
-    }
-
-    /*
-    public ArrayList<Tweet> getAllTweets() {
-        return tweetList;
-    }
-     */
-
     // en metod för att veta vilka otillåtna tecken som finns.
-    public boolean containsInvalidCharacters(String tweet){
+    public boolean containsInvalidCharacters(String thread){
         String invalidCharsRegex = "[\\⛧𖤐⛥♱𐕣⁶⁶⁶⁶𖤐⁶♰𓃶🜏𖤐𐕣⁶⁶⁶☠︎︎🗡⛧☦卐卍\"]";
         Pattern pattern = Pattern.compile(invalidCharsRegex);
         
-        return pattern.matcher(tweet).find();
+        return pattern.matcher(thread).find();
     }
 
-    @PostMapping("/post-tweet")
-    public ResponseEntity<String> postTweet(@RequestBody Map<String, String> userInput){
-        String tweet = userInput.get("tweet");
+    @PostMapping("/post-thread")
+    public ResponseEntity<String> postThread(@RequestBody Map<String, String> userInput){
+        String thread = userInput.get("thread");
 
-        if(checkIfEmpty(tweet)){
-            return ResponseEntity.badRequest().body("The tweet does not exist. Please Try again");
+        if(checkIfEmpty(thread)){
+            return ResponseEntity.badRequest().body("The thread does not exist. Please Try again");
         }
 
-        if(tweetAboveLimit(tweet)){
-            return ResponseEntity.badRequest().body("The tweet has more than 280 characters.");
+        if(threadAboveLimit(thread)){
+            return ResponseEntity.badRequest().body("The thread has more than 500 characters.");
         }
 
-        if(containsInvalidCharacters(tweet)){
+        if(containsInvalidCharacters(thread)){
             System.out.println("Sant");
-            return ResponseEntity.badRequest().body("Error: Tweeten innehåller otillåtna tecken.");
+            return ResponseEntity.badRequest().body("Error: The thread has forbidden charachters.");
         }
 
-        boolean success = sendToTwitterAPI(tweet);
+        boolean success = sendToThreadAPI(thread);
 
         if(success){
-            return ResponseEntity.ok("The tweet has been sent.");
+            return ResponseEntity.ok("The thread has been sent.");
         } else {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("Error: Kunde inte publicera tweeten.");
+                    .body("Error: Cannot publish thread.");
         }
 
     }
 
-    //skickar tweet till twitters API
-    public boolean sendToTwitterAPI(String tweet){
+    //skickar threads till threads API
+    public boolean sendToThreadAPI(String thread){
         try {
             //mock - kod
-            System.out.println("Följande tweet skickas till twitters API: " + tweet);
+            System.out.println("The followign thread is being sent to Threads API: " + thread);
             return true;
         } catch (Exception e){
             System.out.println(e.getMessage());
@@ -86,40 +72,40 @@ public class Controller_Twitter {
 
     // ResponseEntity is a clas used to show HTTP requests, such as status code.
     @PostMapping
-    public ResponseEntity<String> addTweet(@RequestBody Tweet tweet) {
-        if(tweet.getTweetInformation() == null){
-            return ResponseEntity.badRequest().body("The tweet does not exist. Please Try again");
+    public ResponseEntity<String> addThread(@RequestBody Thread thread) {
+        if(thread.getThreadInformation() == null){
+            return ResponseEntity.badRequest().body("The thread does not exist. Please Try again");
         }
-        else if(tweet.getTweetInformation().isEmpty()){
-            return ResponseEntity.badRequest().body("The tweet is empty and therefore cannot be sent. Please try again.");
+        else if(thread.getThreadInformation().isEmpty()){
+            return ResponseEntity.badRequest().body("The thread is empty and therefore cannot be sent. Please try again.");
         }
 
         else {
-            tweetList.add(tweet);
-            return ResponseEntity.ok("Tweet successfully received: " + tweet.getTweetInformation());
+            threadList.add(thread);
+            return ResponseEntity.ok("Thread successfully received: " + thread.getThreadInformation());
         }
     }
 
-    @PostMapping("/manage-tweet")
-    public ResponseEntity<HashMap<String, Object>> manageTweet(@RequestBody Map<String, String> userInput) {
+    @PostMapping("/manage-thread")
+    public ResponseEntity<HashMap<String, Object>> manageThread(@RequestBody Map<String, String> userInput) {
         HashMap<String, Object> spellingControl = new HashMap<>();
-        String userTweet = userInput.get("tweet");
+        String userThread = userInput.get("thread");
         String specified_language = "sv"; ///måste ändras sen, där användaren får välja eget språk.
     
-        boolean empty_tweet = checkIfEmpty(userTweet);
+        boolean empty_thread = checkIfEmpty(userThread);
         boolean no_language_specified = checkIfEmpty(specified_language);
     
-        if (empty_tweet || no_language_specified) {
-            spellingControl.put("invalid", "The tweet is either empty or no language has been specified");
+        if (empty_thread || no_language_specified) {
+            spellingControl.put("invalid", "The thread is either empty or no language has been specified");
             return ResponseEntity.badRequest().body(spellingControl);
         }
     
-        if (containsInvalidCharacters(userTweet)) {
-            spellingControl.put("invalid", "Tweet contains invalid characters.");
+        if (containsInvalidCharacters(userThread)) {
+            spellingControl.put("invalid", "Thread contains invalid characters.");
             return ResponseEntity.badRequest().body(spellingControl);
         }
 
-        HashMap<String, Object> librisResponse = libris.checkSpelling(userTweet, specified_language);
+        HashMap<String, Object> librisResponse = libris.checkSpelling(userThread, specified_language);
 
         if(librisResponse.containsKey("invalid")){
             String key = "invalid";
@@ -131,10 +117,10 @@ public class Controller_Twitter {
 
         Map<String, String> spellingCorrection = suggestedGrammar(librisResponse);
     
-        String correctedTweet = String.join(" ", spellingCorrection.values());
+        String correctedThread = String.join(" ", spellingCorrection.values());
 
-        spellingControl.put("before", userTweet);
-        spellingControl.put("after", correctedTweet);
+        spellingControl.put("before", userThread);
+        spellingControl.put("after", correctedThread);
         spellingControl.put("suggestions", spellingCorrection);
     
         return ResponseEntity.ok(spellingControl);
@@ -148,8 +134,8 @@ public class Controller_Twitter {
         }
     }
 
-    public boolean tweetAboveLimit(String tweet){
-        if(tweet.length() > 280){
+    public boolean threadAboveLimit(String thread){
+        if(thread.length() > 500){
             return true;
         } else {
             return false;
@@ -179,13 +165,5 @@ public class Controller_Twitter {
         return spellingCorrection;
     }
 
-    /* 
-    public static void main(String[] args) {
-        Controller_Twitter controller = new Controller_Twitter();
-        String inputTweet = "Twitetr är fantastisk!";
-        String correctedTweet = controller.suggestedGrammar(inputTweet, "svenska");
-        System.out.println(correctedTweet);
-    }
-        */
 
 }
---
+++ Twitetr/src/main/java/com/example/Twitetr/Entity/Thread.java
@@ -0,0 +1,17 @@
+package com.example.Twitetr.Entity;
+
+public class Thread {
+    private String thread_information;
+
+    public Thread(){
+
+    }
+
+    public String getThreadInformation(){
+        return thread_information;
+    }
+
+    public void setThread_information(String thread_info){
+        this.thread_information = thread_info;
+    }
+}
