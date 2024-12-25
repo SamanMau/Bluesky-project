4bc8c46603fee843b1e6de4800dc9b0766861343
SamanMau <saman_jejo@hotmail.com>
2024-12-26 00:02:31 +0100

    Fixade problem med LIBRIS
    Co-Authored-By: kingabdull02 <161301313+kingabdull02@users.noreply.github.com>

--- Twitetr/src/main/java/com/example/Twitetr/Service/LibrisManager.java
+++ Twitetr/src/main/java/com/example/Twitetr/Service/LibrisManager.java
@@ -1,6 +1,10 @@
 package com.example.Twitetr.Service;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+import io.github.cdimascio.dotenv.Dotenv;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -12,6 +16,8 @@ import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.security.PublicKey;
 import java.util.HashMap;
+
+import javax.xml.parsers.DocumentBuilderFactory;
 
 @Service
 public class LibrisManager {
@@ -25,44 +31,47 @@ public class LibrisManager {
         RestTemplate restTemplate = new RestTemplate();
         
         HashMap<String, Object> map = new HashMap<>();
+        String key = getKey();
         String URL = String.format(LIBRIS_API_URL, URLEncoder.encode(userInput, StandardCharsets.UTF_8), key);
         
+        /*
         System.out.println("LIBRIS API URL: " + URL);
+        System.out.println("nyckel: "+ key);
+        */
     
         try {
-            // Skickar GET-förfrågan
+            // get förfrågan skickas
             ResponseEntity<String> response = restTemplate.getForEntity(URL, String.class);
             
-            // Logga svaret från API
-            System.out.println("LIBRIS API Response: " + response.getBody());
-    
-            // Om du vill granska HTTP-statuskoden
-            System.out.println("HTTP Status Code: " + response.getStatusCode());
-    
-            // Processera API-svaret
             String result = response.getBody();
-            if (result != null) {
-                map = parseJSON(result);
+
+            System.out.println("LIBRIS API Response: " + result);
+    
+            System.out.println("HTTP Status Code: " + result);
+    
+            if(result != null){
+                XmlMapper xml = new XmlMapper();
+                map = xml.readValue(result, HashMap.class);
+            } else {
+                map.put("Invalid", "LIBRIS returned an empty response");
             }
+
         } catch (Exception e) {
             e.printStackTrace();
             map.put("invalid", "LIBRIS API-fel: " + e.getMessage());
         }
-    
+        
         return map;
     }
 
-    public boolean verifyApiKey(){
-        String key = System.getenv("LIBRIS_API_NYCKEL");
+    public String getKey(){
+        Dotenv dotenv = Dotenv.configure()
+                .directory(System.getProperty("user.dir"))
+                .filename(".env")
+                .load();
 
-        if(key != null){
-            return true;
-        }
-
-        if(!key.isEmpty()){
-            return true;
-        }
-
-        return false;
+        String key = dotenv.get("LIBRIS_API_NYCKEL");
+        return key;
     }
+
 }
