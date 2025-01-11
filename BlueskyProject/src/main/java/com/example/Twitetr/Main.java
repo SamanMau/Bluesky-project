1902801b8b44c3239559e1262b7f28ec195c9396
saman <saman_jejo@hotmail.com>
2025-01-11 11:01:52 +0100

    Ändrade namn från "Twitetr" till bluesky

--- BlueskyProject/src/main/java/com/example/Twitetr/Main.java
+++ BlueskyProject/src/main/java/com/example/Twitetr/Main.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TwitetrApplication {
+public class Main {
 	public static void main(String[] args) {
 		System.out.println("Current working directory: " + System.getProperty("user.dir"));
 
-		SpringApplication.run(TwitetrApplication.class, args);
+		SpringApplication.run(Main.class, args);
 			
 	}
 
